b47141f547a1ca364b84a6e6c1dc57856cfc6dbb
caramelloinfo <caramellomari@gmail.com>
2018-03-01 22:40:24 -0300

    alteração do tipo do atributo código

--- angular-app/src/app/disciplina.model.ts
+++ angular-app/src/app/disciplina.model.ts
@@ -1,11 +1,11 @@
 import {Professor} from './professor.model'
 export class Disciplina {
-    codigo: string;
+    codigo: number;
     nome: string;
     professor: Professor;
     descricao: string;
   
-    constructor(codigo: string, nome: string, professor: Professor, descricao?: string) {
+    constructor(codigo: number, nome: string, professor: Professor, descricao?: string) {
       this.codigo = codigo;
       this.nome = nome;
       this.professor = professor;
