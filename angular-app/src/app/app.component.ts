658cf18d6246b88706f7f0b451ccafa960f9dfe5
Laboratórios de Informática <labins@labin.canoas>
2018-03-12 21:48:46 -0300

    anotações

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -64,7 +64,7 @@ export class AppComponent {
     this.codigo = null;
     this.nome = null;
     this.descricao = null;
-    this.limpar();
+    
   }
   criar(){
     const d = new Disciplina(this.codigo, this.nome, this.descricao);
